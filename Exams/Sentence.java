8393cc746d33f7377ecb04bd2f48b1fdbe5a3fc2
Curtis Merrell <cmerrell@ssusd.org>
2018-03-23 07:40:23 -0700

    Adds Written Exam1 Solution

---
+++ Exams/Sentence.java
@@ -0,0 +1,78 @@
+public class Sentence {
+    /** The sentence to manipulate */
+    private String sentence;
+
+    /** @return an ArrayList of integer positions containing a blank in this sentence.
+    * If there are no blanks in the sentence, return an empty list.
+    */
+    public List<Integer> getBlankPositions(){ 
+        ArrayList<Integer> blankPositions = new ArrayList<Integer>();
+        for(int i = 0; i < sentence.length(); i++) {
+            if(sentence.charAt(i) == ' ') {
+                blankPositions.add(i);
+            }
+        }
+
+        return blankPositions;
+    }
+
+    /** @return the number of words in this sentence
+    *   Precondition: Sentence contains at least one word
+    */
+    public int countWords() { 
+        return getBlankPositions().size() + 1;
+    }
+
+    /** @return the array of words in this sentence
+    *   Precondition:
+    *       - Any two words in the sentence are separated by one blank
+    *       - The sentence contains at least one word
+    *   Postcondition: String[] returned containing the words in this sentence
+    */
+    public String[] getWords() { 
+        String[] result = new String[countWords()];
+        ArrayList<Integer> blankPositions = getBlankPositions();
+
+        for(int i = 0; i < blankPositions.size(); i++) {
+            int position = blankPositions.get(i);
+            int prevPosition;
+            if(i == 0) {
+                prevPosition = 0;
+            }
+            else {
+                prevPosition = blankPositions.get(i - 1) + 1;
+            }
+            result[i] = sentence.substring(prevPosition, position - prevPosition);
+        }
+
+        if(blankPositions.size() == 0) {
+            results[0] = sentence;
+        } else {
+            int lastBlank = blankPositions.get(blankPositions.size() - 1);
+            results[results.length - 1] = sentence.substring(lastBlank + 1);
+        }
+
+        return results;
+    }
+
+    public String[] getWordsPartialSolution() {
+        //Pseudocode
+        String[] result = new String[countWords()];
+        ArrayList<Integer> blankPositions = getBlankPositions();
+
+        for(i in blankPositions) {
+            position = blankPositions at i;
+            if(i == 0) {
+                //Boundary case
+            }
+            else {
+                prevPosition = blankPosition at i - 1;
+                results[i] = sentence.substring(prevPosition, position - prevPosition);
+            }
+        }
+        //Get Last Word
+        return results;
+    }
+
+    //Constructor and other methods are not shown.
+}
