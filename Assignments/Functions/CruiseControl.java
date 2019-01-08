a4e6f47b8736e3226fa8e6717483200c2fe1c460
Curtis Merrell <cmerrell@ssusd.org>
2019-01-08 06:45:49 -0800

    Reverts CruiseControl to stub file

--- Assignments/Functions/CruiseControl.java
+++ Assignments/Functions/CruiseControl.java
@@ -5,52 +5,15 @@ class CruiseControl {
 	static int warningSpeed = 30;
 
 	public static void main(String[] args) {
-		toggleCruiseControl();
 
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
-		accelerate();
 	}
 
 	public static void toggleCruiseControl() {
-		cruiseControlSet = !cruiseControlSet;
 
-		if(cruiseControlSet) {
-			System.out.println("Cruise Control Enabled");
-		} else {
-			System.out.println("Cruise Control Disabled");
-			speed = 0;
-		}	
 	}
 
 	public static void accelerate() {
-		if (cruiseControlSet && speed < MAX_SPEED) {
-			speed += 5;
-		}
-		else {
-			System.out.println("Max speed reached.");
-		}
 
-		System.out.println("Current speed: \t" + speed + "mph");
-
-		if(exceedsWarningThreshold(speed)) {
-			alert(false);
-		}
 	}
 
 	public static void decelerate() {
@@ -62,17 +25,10 @@ class CruiseControl {
 	}
 
 	public static void alert(Boolean isDecelerating) {
-		if(isDecelerating) {
-			System.out.println("Speed still exceeds the warning threshold.");
-		}
-		else {
-			System.out.println("Speed exceeds the warning threshold.");			
-		}
-	
-		System.out.println("Threshold: \t" + speed + "/" + warningSpeed + "mph");
+
 	}
 
 	public static Boolean exceedsWarningThreshold(int speed) {
-		return speed > warningSpeed;
+
 	}
 }
